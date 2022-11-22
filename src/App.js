005ce093e07916473c0c094999d9f58798a3e2cd
duranbe <benoit.durand.mail@gmail.com>
2022-11-22 22:46:23 +0100

    Adding sample text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div className='text-white mx-8'>
+        I’m Derek, an astro-engineer based in Tattooine. I like to build X-Wings at
+        <span className="underline decoration-sky-500"> My Company, Inc</span>.
+        Outside of work, I like to <span className="underline decoration-pink-500">watch
+          pod-racing</span> and have <span className="underline decoration-indigo-500">light-saber</span> fights.
+      </div>
     </div>
   );
 }
