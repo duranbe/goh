b0ba5c69e3a81867f6ed24719bd3eea54bd11508
duranbe <benoit.durand.mail@gmail.com>
2022-11-23 18:02:57 +0100

    fix muliple word selection

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import { useState } from 'react';
 
 export default function App() {
 
-  const [data, setData] = useState({ words:[]});
+  const [data, setData] = useState({ words: [] });
 
 
   function handleMouseUp() {
+    
     const eleme = window.getSelection().getRangeAt(0).cloneContents().children;
+
     if (eleme.length > 0) {
+      let tokenArray = []
       for (var i = 0; i < eleme.length; i++) {
-        var token = document.getElementById(eleme[i].id);
+        let token = document.getElementById(eleme[i].id);
         token.className = "px-0.5 underline decoration-sky-500 font-mono";
+        tokenArray.push(token)
       }
+      
+      setData({ ...data, words: data.words.concat(tokenArray) })
+
     } else {
       const singleTokenId = window.getSelection().getRangeAt(0).startContainer.parentNode.id;
       var singleToken = document.getElementById(singleTokenId);
       singleToken.className = "px-0.5 underline decoration-sky-500 font-mono";
-      
-      setData({...data, words: data.words.concat([singleTokenId])})
-      
+
+      setData({ ...data, words: data.words.concat([singleToken]) })
+
     }
-    
+
   }
 
   let text = "I’m Derek, an astro-engineer based in Tattooine. I like to build X-Wings at My Company, Inc."
@@ -35,7 +42,7 @@ export default function App() {
         }
       </div>
       <div className='text-white mx-8 my-2'>
-        {data.words.map((val) => (<span>{val}</span>)) }
+        {data.words.map((val) => (<span key ={val.id}> {val.innerText} </span>))}
       </div>
     </div>
   );
