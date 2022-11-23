122cf3c7ee9bb2218a82f87eac20f1e0e0e341f0
duranbe <benoit.durand.mail@gmail.com>
2022-11-23 11:21:20 +0100

    Finish sample

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ export default function App() {
 
   function handleMouseUp() {
     const eleme = window.getSelection().getRangeAt(0).cloneContents().children;
-    console.log(window.getSelection().getRangeAt(0).startContainer.parentNode.id);
-    console.log("Nb Eleem : " + eleme.length);
+    if(eleme.length>0){
+      for (var i = 0; i < eleme.length; i++) {
+        var token = document.getElementById(eleme[i].id);
+        token.className = "px-0.5 underline decoration-sky-500 font-mono";
+      }
+    }else{
+      const singleTokenId = window.getSelection().getRangeAt(0).startContainer.parentNode.id;
+      var singleToken = document.getElementById(singleTokenId);
+      singleToken.className = "px-0.5 underline decoration-sky-500 font-mono";
+    }
   }
 
   let text = "I’m Derek, an astro-engineer based in Tattooine. I like to build X-Wings at My Company, Inc."
@@ -13,8 +21,8 @@ export default function App() {
   return (
     <div className="App">
       <div className='text-white mx-8 my-2' onMouseUp={handleMouseUp}>
-        {text.split(" ").map((token) => (
-          <span> {token}</span>
+        {text.split(" ").map((token,index) => (
+          <span key={index} id={index} className="px-0.5 font-mono" >{token}</span>
         ))
         }
       </div>
