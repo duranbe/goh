1311e6e41c4762b9f7bdbe96bcb05b9e97d8f37a
duranbe <benoit.durand.mail@gmail.com>
2023-08-11 15:07:13 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,103 @@
-import './App.css';
-import { useState } from 'react';
-import { Token } from './components/Token';
+import "./App.css";
+import { useState } from "react";
+import { Token } from "./components/Token";
 
 export default function App() {
-
   const [selectedToken, setSelectedToken] = useState<Token[]>([]);
   const tokenIdMap = new Map<number, Token>();
-  const tokenAlreadyinData = (token: Token) => { return selectedToken.some((item: Token) => item.id === token.id)}
-  const csvFileHeaders = "id,tokenStartIndex,tokenEndIndex,value\n"
+  const tokenAlreadyinData = (token: Token) => {
+    return selectedToken.some((item: Token) => item.id === token.id);
+  };
+  const csvFileHeaders = "id,tokenStartIndex,tokenEndIndex,value\n";
 
   const download = () => {
-    let csvFileContent = selectedToken.map((token)=>{ return token.toCSVFormat()}).join("")
+    let csvFileContent = selectedToken
+      .map((token) => {
+        return token.toCSVFormat();
+      })
+      .join("");
     const csvContent = `data:text/csv;charset=utf-8,${csvFileHeaders}${csvFileContent}`;
     const encodedURI = encodeURI(csvContent);
     window.open(encodedURI);
   };
 
-
   function handleMouseUp() {
-
     const eleme = window.getSelection()?.getRangeAt(0).cloneContents().children;
     let tokenArray: Token[] = [];
+    const tokenClassName =
+      "px-0.5 underline decoration-sky-500 font-mono underline-offset-4 decoration-2";
 
     if (eleme && eleme.length > 0) {
-
       for (var i = 0; i < eleme.length; i++) {
         let token = document.getElementById(eleme[i].id);
         if (token && token.id) {
-          token.className = "px-0.5 underline decoration-sky-500 font-mono underline-offset-4 decoration-2";
-          let t = Number(token.id)
+          token.className = tokenClassName;
+          let t = Number(token.id);
           let v = tokenIdMap.get(t);
           if (v && !tokenAlreadyinData(v)) {
             tokenArray.push(v);
           }
-
-
         }
-
       }
-
     } else if (eleme) {
-      const parentNode = window.getSelection()?.getRangeAt(0)?.startContainer?.parentNode
+      const parentNode = window.getSelection()?.getRangeAt(0)
+        ?.startContainer?.parentNode;
       //@ts-ignore
       const singleTokenId = parentNode?.id;
       var singleToken = document.getElementById(singleTokenId);
       if (singleToken) {
-        singleToken.className = "px-0.5 underline decoration-sky-500 font-mono underline-offset-4 decoration-2";
-        let t = Number(singleToken.id)
+        singleToken.className = tokenClassName;
+        let t = Number(singleToken.id);
         let v = tokenIdMap.get(t);
         if (v && !tokenAlreadyinData(v)) {
           tokenArray.push(v);
         }
-
-
       }
-
     }
 
     setSelectedToken(selectedToken.concat(tokenArray));
   }
 
-  let text = "I'm Derek, an astro-engineer based in Tattooine. I like to build X-Wings at My Company, Inc."
+  let text =
+    "I'm Derek, an astro-engineer based in Tattooine. I like to build X-Wings at My Company, Inc.";
   let textSplit = text.split(" ");
 
   let tokenArray: Token[] = [];
   var startIndex = 0;
   for (let i = 0; i < textSplit.length; i++) {
-    let newToken = new Token(startIndex,startIndex + textSplit[i].length,textSplit[i],i)
-    tokenArray.push(newToken)
-    tokenIdMap.set(i, newToken)
-    startIndex = startIndex + textSplit[i].length + 1
+    let newToken = new Token(
+      startIndex,
+      startIndex + textSplit[i].length,
+      textSplit[i],
+      i
+    );
+    tokenArray.push(newToken);
+    tokenIdMap.set(i, newToken);
+    startIndex = startIndex + textSplit[i].length + 1;
   }
 
   return (
-  <div className="App">
-    <div className='text-white mx-12 my-2 text-lg' onMouseUp={handleMouseUp} >
-      {text.split(" ").map((token, index) => (<span key={index} id={String(index)} className="px-0.5 font-mono" >{token}</span>))}
+    <div className="App">
+      <div className="text-white mx-12 my-2 text-lg" onMouseUp={handleMouseUp}>
+        {text.split(" ").map((token, index) => (
+          <span key={index} id={String(index)} className="px-0.5 font-mono">
+            {token}
+          </span>
+        ))}
+      </div>
+
+      <div className="text-white mx-8 my-2">
+        {selectedToken.map((val) => (
+          <span key={val.id}>{val.tokenValue} </span>
+        ))}
+      </div>
+
+      <button
+        className="text-white bg-blue-500 px-4 py-2 rounded-sm"
+        onClick={download}
+      >
+        Download
+      </button>
     </div>
-
-    <div className='text-white mx-8 my-2' >
-      {selectedToken.map((val) => (<span key={val.id}>{val.tokenValue} </span>))}
-    </div>
-
-    <button className='text-white bg-blue-500' onClick={download}>Download</button>
-
-
-  </div>
-
-  )
-
-
-
+  );
 }
-
-
